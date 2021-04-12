a3e614cdee8a641cd5502c90a5ba8178fe6a45a0
tanush2611 <76557041+tanush2611@users.noreply.github.com>
2021-04-12 15:47:07 +0530

    Add files via upload

---
+++ sling.js
@@ -0,0 +1,23 @@
+class sling{
+    constructor(bodyA,pointB){
+var options={bodyA:bodyA,pointB:pointB,stiffness:0.04,length:10}
+    
+    this.sling=Constraint.create(options);
+    this.pointB=pointB;
+    World.add(world,this.sling)
+
+}
+fly(){
+    this.sling.bodyA=null
+}
+display(){
+if(this.sling.bodyA){
+
+
+    strokeWeight(5);
+    line(this.sling.bodyA.position.x,this.sling.bodyA.position.y,
+        this.pointB.x,this.pointB.y)
+
+}
+}
+}
